0b7a4a334f0a787f7cf90fae8a0afb83a2b61fa6
Phong Huỳnh <closeheart777@live.com>
2024-12-15 23:34:54 +0700

    feat: new scene

--- src/script.js
+++ src/script.js
@@ -45,7 +45,6 @@ const camera = new THREE.PerspectiveCamera(
   10000
 );
 camera.position.set(34, 16, -20);
-camera.lookAt(106, 5.8, -28);
 scene.add(camera);
 
 /////////////////////////////////////////////////////////////////////////
@@ -69,7 +68,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const ambient = new THREE.AmbientLight(0xa0a0fc, 0.82);
 scene.add(ambient);
 
-const sunLight = new THREE.DirectionalLight(0xe8c37b, 1.96);
+const sunLight = new THREE.DirectionalLight(0xffffff, 1);
 //#region //*shadow
 // sunLight.castShadow = true;
 
@@ -83,15 +82,26 @@ const sunLight = new THREE.DirectionalLight(0xe8c37b, 1.96);
 // sunLight.shadow.camera.top = 100;
 // sunLight.shadow.camera.bottom = -100;
 //#endregion
-sunLight.position.set(-69, 44, 14);
+sunLight.position.set(23, 44, 14);
 scene.add(sunLight);
+
+const additionalLight = new THREE.DirectionalLight(0xffffff, 2);
+additionalLight.position.set(50, 50, 50);
+scene.add(additionalLight);
+
+const light2 = new THREE.DirectionalLight(0xffffff, 2);
+light2.position.set(-50, 50, -50);
+scene.add(light2);
+
+// const hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x444444, 1); // Sky color, ground color, intensity
+// scene.add(hemisphereLight);
 
 /////////////////////////////////////////////////////////////////////////
 ///// LOADING GLB/GLTF MODEL FROM BLENDER
 let modelCenter = new THREE.Vector3(); // Vector to store the center of the model
 
 let mixer; // Declare mixer outside to access it in the render loop
-loader.load("models/gltf/danon.glb", function (gltf) {
+loader.load("models/gltf/highway-corner.glb", function (gltf) {
   const model = gltf.scene; // Get the scene (3D model)
   scene.add(gltf.scene);
 
@@ -99,14 +109,14 @@ loader.load("models/gltf/danon.glb", function (gltf) {
   //   const boundingBox = addBoundingBox(model);
   const box = new THREE.Box3().setFromObject(model);
   //   box.min.
-  modelCenter = box.getCenter(new THREE.Vector3());
+  let temp = box.getCenter(new THREE.Vector3());
   //   console.log(modelCenter, center);
   //   mesh.position.set(
   //     mesh.position.x - center.x,
   //     mesh.position.y - center.y,
   //     mesh.position.z - center.z
   //   );
-
+  modelCenter = new THREE.Vector3(temp.x, box.min.y, temp.z);
   // Update the camera to look at the calculated center
   camera.lookAt(modelCenter);
   controls.target.copy(modelCenter);
@@ -120,7 +130,11 @@ loader.load("models/gltf/danon.glb", function (gltf) {
       action.play(); // Play the animation
     });
   }
-
+  introAnimation(); // call intro animation on start
+  playAudio();
+  setTimeout(() => {
+    playHornEffectAudio();
+  }, 2000);
   // Assuming the GLTF model contains meshes
   //   gltf.scene.traverse((child) => {
   //     if (child.isMesh) {
@@ -135,13 +149,13 @@ loader.load("models/gltf/danon.glb", function (gltf) {
 function introAnimation() {
   controls.enabled = false; //disable orbit controls to animate the camera
 
-  new TWEEN.Tween(camera.position.set(26, 4, -35))
+  new TWEEN.Tween(camera.position.set(34, 16, -20))
     .to(
       {
         // from camera position
-        x: -115, //desired x position to go
-        y: 109, //desired y position to go
-        z: 199, //desired z position to go
+        x: 25, //desired x position to go
+        y: 200, //desired y position to go
+        z: 461, //desired z position to go
       },
       6500
     ) // time take to animate
@@ -157,8 +171,6 @@ function introAnimation() {
       TWEEN.remove(this); // remove the animation from memory
     });
 }
-
-introAnimation(); // call intro animation on start
 
 /////////////////////////////////////////////////////////////////////////
 //// DEFINE ORBIT CONTROLS LIMITS
@@ -194,41 +206,63 @@ function rendeLoop() {
 
 rendeLoop(); //start rendering
 
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js'
-const gui = new GUI()
+// import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
+// const gui = new GUI();
 
-// create parameters for GUI
-var params = {color: sunLight.color.getHex(), color2: ambient.color.getHex(), color3: scene.background.getHex()}
+// // create parameters for GUI
+// var params = {
+//   color: sunLight.color.getHex(),
+//   color2: ambient.color.getHex(),
+//   color3: scene.background.getHex(),
+// };
 
-// create a function to be called by GUI
-const update = function () {
-	var colorObj = new THREE.Color( params.color )
-	var colorObj2 = new THREE.Color( params.color2 )
-	var colorObj3 = new THREE.Color( params.color3 )
-	sunLight.color.set(colorObj)
-	ambient.color.set(colorObj2)
-	scene.background.set(colorObj3)
-}
+// // create a function to be called by GUI
+// const update = function () {
+//   var colorObj = new THREE.Color(params.color);
+//   var colorObj2 = new THREE.Color(params.color2);
+//   var colorObj3 = new THREE.Color(params.color3);
+//   sunLight.color.set(colorObj);
+//   ambient.color.set(colorObj2);
+//   scene.background.set(colorObj3);
+// };
 
-//////////////////////////////////////////////////
-//// GUI CONFIG
-gui.add(sunLight, 'intensity').min(0).max(10).step(0.0001).name('Dir intensity')
-gui.add(sunLight.position, 'x').min(-100).max(100).step(0.00001).name('Dir X pos')
-gui.add(sunLight.position, 'y').min(0).max(100).step(0.00001).name('Dir Y pos')
-gui.add(sunLight.position, 'z').min(-100).max(100).step(0.00001).name('Dir Z pos')
-gui.addColor(params,'color').name('Dir color').onChange(update)
-gui.addColor(params,'color2').name('Amb color').onChange(update)
-gui.add(ambient, 'intensity').min(0).max(10).step(0.001).name('Amb intensity')
-gui.addColor(params,'color3').name('BG color').onChange(update)
+// //////////////////////////////////////////////////
+// //// GUI CONFIG
+// gui
+//   .add(sunLight, "intensity")
+//   .min(0)
+//   .max(10)
+//   .step(0.0001)
+//   .name("Dir intensity");
+// gui
+//   .add(sunLight.position, "x")
+//   .min(-100)
+//   .max(100)
+//   .step(0.00001)
+//   .name("Dir X pos");
+// gui.add(sunLight.position, "y").min(0).max(100).step(0.00001).name("Dir Y pos");
+// gui
+//   .add(sunLight.position, "z")
+//   .min(-100)
+//   .max(100)
+//   .step(0.00001)
+//   .name("Dir Z pos");
+// gui.addColor(params, "color").name("Dir color").onChange(update);
+// gui.addColor(params, "color2").name("Amb color").onChange(update);
+// gui.add(ambient, "intensity").min(0).max(10).step(0.001).name("Amb intensity");
+// gui.addColor(params, "color3").name("BG color").onChange(update);
 
-//////////////////////////////////////////////////
-//// ON MOUSE MOVE TO GET CAMERA POSITION
-document.addEventListener('mousemove', (event) => {
-    event.preventDefault()
+// //////////////////////////////////////////////////
+// //// ON MOUSE MOVE TO GET CAMERA POSITION
+// document.addEventListener(
+//   "mousemove",
+//   (event) => {
+//     event.preventDefault();
 
-    console.log(camera.position)
-
-}, false)
+//     console.log(camera.position);
+//   },
+//   false
+// );
 
 //// HELPER FUNCTION TO ADD A BOUNDING BOX
 function addBoundingBox(object) {
@@ -245,3 +279,75 @@ function calculateCenter(min, max) {
     (min.z + max.z) / 2
   );
 }
+let audioContext;
+let audioBuffer;
+let hornEffectBuffer;
+let source;
+let gainNode;
+
+async function loadAudio(url) {
+  // Initialize AudioContext
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+  // Fetch audio data
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+
+  // Decode audio data
+  audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+}
+
+async function loadHornEffect(url) {
+  // Initialize AudioContext
+  if(!audioContext)
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+  // Fetch audio data
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+
+  // Decode audio data
+  hornEffectBuffer = await audioContext.decodeAudioData(arrayBuffer);
+}
+
+function playAudio() {
+  if (!audioBuffer) return;
+
+  // Create audio buffer source
+  source = audioContext.createBufferSource();
+  source.loop = true; // Enable looping
+
+  source.buffer = audioBuffer;
+  source.connect(audioContext.destination);
+
+  // Start playback
+  source.start(0);
+}
+
+function playHornEffectAudio() {
+  if (!hornEffectBuffer) return;
+
+  // Create audio buffer source
+  source = audioContext.createBufferSource();
+  source.loop = true; // Enable looping
+  gainNode = audioContext.createGain();
+
+  source.buffer = hornEffectBuffer;
+  // source.connect(audioContext.destination);
+  source.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+
+  gainNode.gain.setValueAtTime(0.3, audioContext.currentTime); // Start with low volume
+  // Start playback
+  source.start(0);
+}
+
+function stopAudio() {
+  if (source) {
+    source.stop();
+  }
+}
+
+// Load the WebA file
+loadAudio("musics/videoplayback.weba"); // Update with the actual file path
+loadHornEffect("musics/train.mp3"); // Update with the actual file path
